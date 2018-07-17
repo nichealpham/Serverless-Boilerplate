235a1b3d27433bdcecdad0da0a9a6be4e78fc390
nguyenpham95 <phamkhoinguyen1995@gmail.com>
2018-07-17 12:32:30 +0700

    Update name functions

--- src/handler.ts
+++ src/handler.ts
@@ -3,6 +3,6 @@ import {HandlerHelper} from './system/app/helper/HandlerHelper';
 
 export async function hello (event, context, callback) {
     let greeting = new Greeting('Welcome to my Serverless Template !!!');
-    const response = HandlerHelper.successResponse(200, null, greeting.message);
+    let response = HandlerHelper.createSuccessResponse(200, null, greeting.message);
     callback(null, response);
 };
--- src/system/app/helper/HandlerHelper.ts
+++ src/system/app/helper/HandlerHelper.ts
@@ -14,13 +14,21 @@ export class HandlerHelper {
         });
     }
 
-    static successResponse(code = 200, data?: any, message?: String) {
-        let body: any = {};
+    static createSuccessResponse(code = 200, data?: any, message: String = 'Success') {
+        let body: any = {
+            message
+        };
         if (data) body.data = data;
-        if (message) body.message = message;
         return {
             statusCode: code,
             body: JSON.stringify(body),
         };
+    }
+
+    static createErrorResponse(code = 400, message: String = 'An error occured during the process!') {
+        return {
+            statusCode: code,
+            error: message
+        }
     }
 }
